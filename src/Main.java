b30fca6ef40753c31ec7d3149a90dbdea31ffbd3
Veronazavr <verochka10@list.ru>
2020-08-16 22:26:14 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-       int precent = (int) 0.1;
-       long amount = 1000;
-       long bonus = amount * precent / 100;
+       int present = (int) 1;
+       long amount = 800;
+       long bonus = (long) ((amount * present) / 100);
        long limit = 1000;
-       if (amount > limit) {
+        if (amount > limit) {
            amount = limit;
-       }
+            System.out.println(bonus);
+
+        }
 
     }
+
+
 }
